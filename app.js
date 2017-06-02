748f4e486b315a610943ae9f5aa804dd5acd9853
Yair <yairco1990@gmail.com>
2017-06-02 18:04:02 +0300

    improve code

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(express.static(__dirname + '/node_modules'));
 Util.log("Server running...");
 ////////////////////end of server stuff////////////////////
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //init DB
 var DBManager = require('./dal/DBManager');
 
@@ -52,12 +58,6 @@ io.on('connection', function (socket) {
 
 //launch cleaner
 require('./cleaner')(connections);
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
